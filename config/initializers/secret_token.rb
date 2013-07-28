975bdeaeb9fa911fd916371d6b8103c445148024
Vipper <vipper1989@gmail.com>
2013-07-27 22:41:35 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrientacaoPedagogicaEscolar::Application.config.secret_key_base = 'f47bbe298efcc2549d82e3b93a3bf88a33e06847da786b13c7f46b678d3ad8d1cee22b8e11c0499165217e1462ae78d066d61416ec7fec99d8b30935f72086a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OrientacaoPedagogicaEscolar::Application.config.session_store :cookie_store, key: '_orientacao_pedagogica_escolar_session'
